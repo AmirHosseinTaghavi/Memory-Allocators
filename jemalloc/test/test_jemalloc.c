08dcf2c15579912aaa59f3ce3afcd9bd9c6c6234
amirhtk1 <a.taghavi74@gmail.com>
2019-05-12 21:01:53 +0430

    test files

---
+++ jemalloc/test/test_jemalloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(){
+
+	int *ptr;
+	int num = 25000000;
+
+	clock_t begin = clock();
+	ptr = (int*) malloc(num * sizeof(int));
+	clock_t end = clock();
+	double time_spent_first = (double) (end - begin) / CLOCKS_PER_SEC;
+
+	printf("time spent first= %f \n", time_spent_first);
+
+	clock_t begin1 = clock();
+	for(int i=0; i< num; i++){
+		ptr[i] = i%10000;
+	}
+	clock_t end1 = clock();
+	double time_spent_allocation = (double) (end1 - begin1) / CLOCKS_PER_SEC;
+
+	printf("time spent while allocation= %f \n", time_spent_allocation);
+
+	sleep(3);
+
+	free(ptr);
+	return 0;
+}
