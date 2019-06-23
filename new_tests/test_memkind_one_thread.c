fc6d08482fad893bd92bcfece2a53538a71a01cb
amirhtk1 <a.taghavi74@gmail.com>
2019-06-23 12:22:49 +0430

    rm random

--- new_tests/test_memkind_one_thread.c
+++ new_tests/test_memkind_one_thread.c
@@ -5,16 +5,18 @@
 #include <memkind.h>
 	 
 #define NALLOCATIONS 334
-#define MINBYTES 3000000
-#define MAXBYTES 3000000
+/// objbytes 3000 kb
+// run with ./test_memkind_one 3000
 
-int main(){
+int main(int argc, char* argv[]){
+	// in kb
+	int object_size = atoi(argv[1]);
+
 	clock_t begin = clock();
 	int* allocs[NALLOCATIONS];
 	for(int i=0; i<NALLOCATIONS; i++){
-		int random_size = (rand() % (MAXBYTES - MINBYTES + 1)) + MINBYTES;
 		// memory allocation
-		allocs[i] = (int*) memkind_malloc(MEMKIND_HUGETLB, random_size);
+		allocs[i] = (int*) memkind_malloc(MEMKIND_HUGETLB, object_size*1000);
 		// memory usage
 		for(int k=0; k<750000; k++){
 			allocs[i][k]=10;
